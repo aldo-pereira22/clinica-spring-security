3e0f54f2463f8ca389eed9822246922f2a91f912
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-08-01 20:26:24 -0300

    Listagem de usuários

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,18 +1,43 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
+	
 //	Abrir cadastro de usuários (medico/admin/paciente)
 	@GetMapping("/novo/cadastro/usuario")
 	public String cadastroPorAdminParaAdminMedicoPaciente(Usuario usuario) {
 		return "usuario/cadastro";
+	}
+
+	
+//	Abrir lista de usuários de usuários
+	@GetMapping("/lista")
+	public String listarUsuarios() {
+		
+		return "usuario/lista";
+	}
+
+	
+// Listar usuários na datatables
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listarUsuariosDataTables(HttpServletRequest request) {
+		
+		return ResponseEntity.ok(service.buscarTodos(request)) ;
 	}
 }
