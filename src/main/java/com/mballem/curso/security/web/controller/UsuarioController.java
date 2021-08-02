39cf2453775f4193b092f6a6df9a695fe57c7cd5
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-08-02 18:41:57 -0300

    Conversor para lista de perfis

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,4 +39,33 @@ public class UsuarioController {
 		
 		return ResponseEntity.ok(service.buscarTodos(request)) ;
 	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
